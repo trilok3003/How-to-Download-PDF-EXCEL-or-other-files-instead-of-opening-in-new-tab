2a6e764112c1f8272283f55cafbbba0991f9650f
trilok3003 <triloksingh3003@gmail.com>
2021-07-29 23:08:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-zuaitp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zuaitp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  myFileName = 'my-custom-file.pdf';
+  fileUrl = 'assets/Docs/sample.pdf'
+}
